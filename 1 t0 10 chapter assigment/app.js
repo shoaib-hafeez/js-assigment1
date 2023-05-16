3616eeba2ea07744366c5e6ca333f03964d75028
Your Nameshoaib-hafeez <shoaibhafeez94@gmail.com>
2023-05-17 03:50:26 +0500

    6 chapter assigment

---
+++ 1 t0 10 chapter assigment/app.js
@@ -0,0 +1,87 @@
+// alert  1st task
+
+var fn = prompt("first name");
+var ln = prompt("last name");
+var email = prompt("email");
+var phone = prompt("phone#");
+var password = prompt("password");
+
+alert(fn + " " + ln + " " + email + " " + phone + " " + password);
+
+// 2nd task
+
+alert("You're learning JavaScript!");
+
+// task 3
+
+alert("hello world!");
+
+// (chapter #2)
+
+var firstName;
+var productType;
+
+firstName = "shoaib";
+
+var teamName = "karachiking";
+
+alert(teamName);
+
+var bestMan = "Charlie";
+
+bestMan = "jhon";
+
+// (chapter #3)
+
+var case1;
+case1 = 144;
+
+var num = 9;
+var num1 = num + 2;
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge; //ans is 110
+
+// (chapter #4)
+
+var productCost = 3.45;
+
+var nameOfBand;
+
+var newNum = 123;
+
+var firstName = "shoaib";
+var lastName = "hafeez";
+var fullName = firstName + " " + lastName;
+
+// (chapter #5)
+
+//%
+var num = 20 % 6; // ans is 2
+
+var largNum = 1000 * 2000;
+
+var num1 = 5;
+var num2 = 4;
+var result = num1 - num2;
+
+alert(num1 * num2);
+
+// (chapter #6)
+
+var x = 2 ;
+
+var x = x + 1;
+
+var y = 100
+y--
+
+var x = 50;
+var y = x++; // ans is 51
+
+var y = 50;
+var z = --y; //ans is 49
+
+var x = 50;
+var y = x++;
